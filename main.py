85c4641a83f5de1e0afccb0dcead63d2acb43da2
parthu <parthasaradhimunakala@gmail.com>
2023-04-08 08:39:22 +0530

    third commit

--- main.py
+++ main.py
@@ -1,17 +1,115 @@
-from flask import Flask,render_template
+from flask import Flask,Response,request,jsonify,render_template
+import pymongo,json,jwt
+from flask_bcrypt import Bcrypt
+from datetime import datetime,timedelta
+import pandas as pd
+import flask_excel as excel
+
+
+# Opening config file to change according to the user
+with open('config.json','r') as c:
+    params = json.load(c)['params']
 
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
+secret = "***************"
 
+#connecting with mongodb server
 
+try:
+    host = params['host']
+    port = params['port']
+    timeout = params['timeout']
+    mongo = pymongo.MongoClient(host=host,port=port,serverSelectionTimeoutMS=timeout)
+    db = mongo.Attendance # Creates a database of Attendance
+    mongo.server_info() # This will remind the server to check after the timeout and get info
+
+except Exception as ex:
+    print(ex)
+    
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/auth')
-def auth():
-    return render_template("auth.html")
+@app.route('/auth/<string:prof>')
+def auth(prof):
+    return render_template('auth.html',v=prof)
 
+@app.route('/signup/<string:x>', methods=['POST'])
+def save(x):
+    message = ""
+    code = 500
+    status = "fail"
+    try:
+        data = request.get_json()
+        check = db[x].count_documents({"email": data['email']})
+        if check>= 1:
+            message = "user with that email exists"
+            code = 401
+            status = "fail"
+        else:
+            # hashing the password so it's not stored in the db as it was 
+            data['password'] = bcrypt.generate_password_hash(data['password'])
+            data['created'] = datetime.now()
+
+            #this is bad practice since the data is not being checked before insert
+            res = db[x].insert_one(data) 
+            if res.acknowledged:
+                status = "successful"
+                message = "user created successfully"
+                code = 201
+    except Exception as ex:
+        message = f"{ex}"
+        status = "fail"
+        code = 500
+    return jsonify({'status': status, "message": message}), 200
+
+#Route to Login for students 
+@app.route('/login/<string:x>', methods=['POST'])
+def login(x):
+    message = ""
+    res_data = {}
+    code = 500
+    status = "fail"
+    try:
+        data = request.get_json()
+        user = db[x].find_one({"email": f'{data["email"]}'})
+
+        if user:
+            user['_id'] = str(user['_id'])
+            if user and bcrypt.check_password_hash(user['password'], data['password']):
+                time = datetime.utcnow() + timedelta(hours=24)
+                token = jwt.encode({
+                        "user": {
+                            "email": f"{user['email']}",
+                            "id": f"{user['_id']}",
+                        },
+                        "exp": time
+                    },secret)
+
+                del user['password']
+
+                message = f"user authenticated"
+                code = 200
+                status = "successful"
+                res_data['token'] = token
+                res_data['user'] = user
+
+            else:
+                message = "wrong password"
+                code = 401
+                status = "fail"
+        else:
+            message = "invalid login details"
+            code = 401
+            status = "fail"
+
+    except Exception as ex:
+        message = f"{ex}"
+        code = 500
+        status = "fail"
+    return jsonify({'status': status, "data": res_data, "message":message}), code
 
 app.run(debug=True)
